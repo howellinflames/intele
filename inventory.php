a6c81067040e50940c5f149c1aa9b49d86359a53
Unknown <micaisnotonfire@gmail.com>
2018-07-24 01:17:26 +0800

    inventory test

--- inventory.php
+++ inventory.php
@@ -11,7 +11,8 @@
 
 
 <?php
-$con = mysql_connect("localhost","root","");
+$con=mysqli_init(); //mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+	mysqli_real_connect($con, "websyst.mysql.database.azure.com", "snazzyhowell@websyst", "buttercup1.", "db_finals", "3306");
 if (!$con)
   {
   die('Could not connect: ' . mysqli_error());
