aae1b285b25a94ddd25f8b4bb19761f2a007d24b
Unknown <micaisnotonfire@gmail.com>
2018-07-24 00:57:19 +0800

    con again

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,10 +1,10 @@
 <?php
 	/*$connect=mysqli_connect("localhost","root","") or die("Unable to connect");*/
 	
-	$connect=mysqli_init(); //mysqli_ssl_set($connect, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-	mysqli_real_connect($connect, "websyst.mysql.database.azure.com", "snazzyhowell@websyst", "buttercup1.", "db_finals", "3306");
+	$connect=mysqli_init(); //mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+	mysqli_real_connect($con, "websyst.mysql.database.azure.com", "snazzyhowell@websyst", "buttercup1.", "db_finals", "3306");
 
-	mysqli_select_db($connect,"db_finals");
+	mysqli_select_db($con,"db_finals");
 	if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 	}
